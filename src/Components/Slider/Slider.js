aba08440edba6d6149c88916843bc5058914b98e
RRRKotov <remich1999@mail.ru>
2020-12-28 21:59:59 +0300

    added flexibility to the app

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./Slider.css";
 import ImgComp from "../ImgComp/ImgComp";
 import i1 from "../../img/i1.jpg";
@@ -8,65 +8,49 @@ import i4 from "../../img/i4.jpg";
 import i5 from "../../img/i5.png";
 import left from "../../img/left.png";
 import right from "../../img/right.png";
-import styles from "./Slider.css"
-
+import styles from "./Slider.css";
 
 const Slider = () => {
-  console.log(styles);
-  
   const [x, setX] = useState(0);
 
-  let sliderArr = [
-    <ImgComp src={i1} />,
-    <ImgComp src={i2} />,
-    <ImgComp src={i3} />,
-    <ImgComp src={i4} />,
-    <ImgComp src={i5} />,
-  ];
+  const images = [i1, i2, i3, i4, i5];
+
+  const NUMBER_OF_SLIDES = 5;
+
+  let sliderArr = [];
+
+  images.forEach((item, index) => {
+    if (index < NUMBER_OF_SLIDES) {
+      sliderArr.push(<ImgComp src={item} />);
+    }
+  });
+
+  const SLIDERS_NUMBER = 100;
 
   const goLeft = () => {
-    x === 0 ? setX(-100 * (sliderArr.length - 1)) : setX(x + 100);
+    x === 0
+      ? setX(-SLIDERS_NUMBER * (sliderArr.length - 1))
+      : setX(x + SLIDERS_NUMBER);
   };
 
   const goRight = () => {
-    x === -100 * (sliderArr.length - 1) ? setX(0) : setX(x - 100);
+    x === -SLIDERS_NUMBER * (sliderArr.length - 1)
+      ? setX(0)
+      : setX(x - SLIDERS_NUMBER);
   };
 
   const dotClick = (value) => {
     setX(value);
   };
 
-
   useEffect(() => {
-    
-    
     let circles = document.querySelectorAll(".Slider__dot");
-    
-    const clearOpac=()=>{
-      circles.forEach(element=>element.style.opacity=0.6)
-    }
-    switch (x) {
-      case 0:
-        clearOpac()
-        circles[0].style.opacity = 1;
-        break;
-      case -100:
-        clearOpac()
-        circles[1].style.opacity = 1;
-        break;
-      case -200:
-        clearOpac()
-        circles[2].style.opacity = 1;
-        break;
-      case -300:
-        clearOpac()
-        circles[3].style.opacity = 1;
-        break;
-      case -400:
-        clearOpac()
-        circles[4].style.opacity = 1;
-        break;
-    }
+    const clearOpac = () => {
+      circles.forEach((element) => (element.style.opacity = 0.6));
+    };
+    clearOpac();
+    let index = -x / SLIDERS_NUMBER;
+    circles[index].style.opacity = 1;
   }, [x]);
 
   return (
@@ -84,19 +68,30 @@ const Slider = () => {
               </div>
             );
           })}
-          <button className={styles.button+" "+styles.left} onClick={goLeft}>
-            <img src={left} className={styles.arrow}/>
+          <button
+            className={styles.button + " " + styles.left}
+            onClick={goLeft}
+          >
+            <img src={left} className={styles.arrow} />
           </button>
-          <button className={styles.button+" "+styles.right} onClick={goRight}>
-          <img src={right} className={styles.arrow}/>
+          <button
+            className={styles.button + " " + styles.right}
+            onClick={goRight}
+          >
+            <img src={right} className={styles.arrow} />
           </button>
         </div>
         <div className={styles.circles}>
-          <div onClick={() => dotClick(0)} className={styles.dot}></div>
-          <div onClick={() => dotClick(-100)} className={styles.dot}></div>
-          <div onClick={() => dotClick(-200)} className={styles.dot}></div>
-          <div onClick={() => dotClick(-300)} className={styles.dot}></div>
-          <div onClick={() => dotClick(-400)} className={styles.dot}></div>
+          {sliderArr.map((item, index) => {
+            const newIndex = -index * SLIDERS_NUMBER;
+            return (
+              <div
+                key={index}
+                className={styles.dot}
+                onClick={() => dotClick(newIndex)}
+              ></div>
+            );
+          })}
         </div>
       </div>
     </div>
